f7b267a00a43d2721b7804460270402020d8d505
iwanka fenix <wwweberfenix@outlook.com>
2023-02-18 19:55:45 +0100

    dzień 11 praca domowa

---
+++ day11/day11/Program.cs
@@ -0,0 +1,33 @@
+﻿using day11;
+
+var employee = new Employee("Wiktoria", "Webow");
+
+employee.AddGrade("Mery");
+employee.AddGrade("6000");
+employee.AddGrade(16);
+employee.AddGrade(13);
+employee.AddGrade(12);
+
+Console.WriteLine("Wyniki dla Foreach ↓");
+var statistics = employee.ForeachStatistics();
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max: {statistics.Max}");
+Console.WriteLine($"Min: {statistics.Min}");
+
+Console.WriteLine("Wyniki dla For ↓ ");
+statistics = employee.ForStatistics();
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max: {statistics.Max}");
+Console.WriteLine($"Min: {statistics.Min}");
+
+Console.WriteLine("Wyniki dla DoWhile ↓ ");
+statistics = employee.DoWhileStatistics();
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max: {statistics.Max}");
+Console.WriteLine($"Min: {statistics.Min}");
+
+Console.WriteLine("Wyniki dla While ↓ ");
+statistics = employee.WhileStatisticsWhile();
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max: {statistics.Max}");
+Console.WriteLine($"Min: {statistics.Min}");
---
+++ day11/day11/Statistics.cs
@@ -0,0 +1,11 @@
+﻿using day11;
+
+namespace day11
+{
+    public class Statistics
+    {
+        public float Min { get; set; }
+        public float Max { get; set; }
+        public float Average { get; set; }
+    }
+}
