c6b8c43f06e85f5e7a9f12a4fd974c3784f25baf
Zharkowaw <124395572+Zharkowaw@users.noreply.github.com>
2023-10-15 12:50:32 +0200

    Delete challengeApp directory

--- challengeApp/challengeApp/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using challengeApp;
-
-var employee = new Employee("Mery", "Webów");
-employee.AddGrade(2);
-employee.AddGrade(2);
-employee.AddGrade(2);
-var statistics = employee.GetStatistics():
-Console.WriteLine($"Average{GetStatistics} ");
